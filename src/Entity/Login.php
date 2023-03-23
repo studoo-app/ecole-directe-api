25023dad79d7101c49eaaf919e3bf6ea9f29b2bc
Benoit Foujols <benoit@foujols.com>
2023-03-23 15:42:45 +0100

    Fix final github Action

--- src/Entity/Login.php
+++ src/Entity/Login.php
@@ -28,6 +28,61 @@ class Login
      */
     private string $idLogin;
 
+    /**
+     * ID de l'utilisateur
+     * @var string
+     */
+    private string $id;
+
+    /**
+     * UID de l'utilisateur
+     * @var string
+     */
+    private string $uid;
+
+    /**
+     * Identifiant de l'utilisateur
+     * @var string
+     */
+    private string $identifiant;
+
+    /**
+     * Type de compte
+     * @var string
+     */
+    private string $typeCompte;
+
+    /**
+     * Email de l'utilisateur
+     * @var string
+     */
+    private string $email;
+
+    /**
+     * Nom et Prenom de l'utilisateur
+     * @var string
+     */
+    private string $nom;
+    private string $prenom;
+
+    /**
+     * Année scolaire courante
+     * @var string
+     */
+    private string $anneeScolaireCourante;
+
+    /**
+     * Nom de l'établissement
+     * @var string
+     */
+    private string $nomEtablissement;
+
+    /**
+     * Profil de l'utilisateur
+     * @var array
+     */
+    private array $profile;
+
 
     public function getToken(): string
     {
@@ -50,6 +105,186 @@ class Login
     {
         $this->idLogin = $idLogin;
 
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param string $id
+     * @return Login
+     */
+    public function setId(string $id): Login
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUid(): string
+    {
+        return $this->uid;
+    }
+
+    /**
+     * @param string $uid
+     * @return Login
+     */
+    public function setUid(string $uid): Login
+    {
+        $this->uid = $uid;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getIdentifiant(): string
+    {
+        return $this->identifiant;
+    }
+
+    /**
+     * @param string $identifiant
+     * @return Login
+     */
+    public function setIdentifiant(string $identifiant): Login
+    {
+        $this->identifiant = $identifiant;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTypeCompte(): string
+    {
+        return $this->typeCompte;
+    }
+
+    /**
+     * @param string $typeCompte
+     * @return Login
+     */
+    public function setTypeCompte(string $typeCompte): Login
+    {
+        $this->typeCompte = $typeCompte;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param string $email
+     * @return Login
+     */
+    public function setEmail(string $email): Login
+    {
+        $this->email = $email;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNom(): string
+    {
+        return $this->nom;
+    }
+
+    /**
+     * @param string $nom
+     * @return Login
+     */
+    public function setNom(string $nom): Login
+    {
+        $this->nom = $nom;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPrenom(): string
+    {
+        return $this->prenom;
+    }
+
+    /**
+     * @param string $prenom
+     * @return Login
+     */
+    public function setPrenom(string $prenom): Login
+    {
+        $this->prenom = $prenom;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAnneeScolaireCourante(): string
+    {
+        return $this->anneeScolaireCourante;
+    }
+
+    /**
+     * @param string $anneeScolaireCourante
+     * @return Login
+     */
+    public function setAnneeScolaireCourante(string $anneeScolaireCourante): Login
+    {
+        $this->anneeScolaireCourante = $anneeScolaireCourante;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNomEtablissement(): string
+    {
+        return $this->nomEtablissement;
+    }
+
+    /**
+     * @param string $nomEtablissement
+     * @return Login
+     */
+    public function setNomEtablissement(string $nomEtablissement): Login
+    {
+        $this->nomEtablissement = $nomEtablissement;
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function getProfile(): array
+    {
+        return $this->profile;
+    }
+
+    /**
+     * @param array $profile
+     * @return Login
+     */
+    public function setProfile(array $profile): Login
+    {
+        $this->profile = $profile;
         return $this;
     }
 }
--- tests/Entity/LoginTest.php
+++ tests/Entity/LoginTest.php
@@ -9,14 +9,52 @@ class LoginTest extends TestCase
 {
     public function testGetToken()
     {
-        $login = new Login('token');
-        $this->assertEquals('token', $login->getToken());
+        $login = new Login();
+        $login->setToken('f6897f82-c10c-42d9-80e8-5c62a4c2acd2');
+        $this->assertEquals('f6897f82-c10c-42d9-80e8-5c62a4c2acd2', $login->getToken());
     }
 
-    public function testSetToken()
+    public function testSetIdLogin()
     {
-        $login = new Login('token');
-        $login->setToken('newToken');
-        $this->assertEquals('newToken', $login->getToken());
+        $login = new Login();
+        $login->setIdLogin('345');
+        $this->assertEquals('345', $login->getIdLogin());
     }
+
+    public function testGetIdLogin()
+    {
+        $login = new Login();
+        $login->setIdLogin('345');
+        $this->assertEquals('345', $login->getIdLogin());
+    }
+
+    public function testGetId()
+    {
+        $login = new Login();
+        $login->setId('345');
+        $this->assertEquals('345', $login->getId());
+    }
+
+    public function testSetId()
+    {
+        $login = new Login();
+        $login->setId('345');
+        $this->assertEquals('345', $login->getId());
+    }
+
+    public function testGetUid()
+    {
+        $login = new Login();
+        $login->setUid('345');
+        $this->assertEquals('345', $login->getUid());
+    }
+
+    public function testSetUid()
+    {
+        $login = new Login();
+        $login->setUid('345');
+        $this->assertEquals('345', $login->getUid());
+    }
+
+
 }

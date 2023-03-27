ee9fbb46aeb095c8eb6cabf729643a06e3ae283e
Benoit Foujols <benoit@foujols.com>
2023-03-27 09:38:42 +0200

    #18 add exemple via Form

--- examples/index.php
+++ examples/index.php
@@ -1,19 +1,5 @@
 <?php
 
-require_once __DIR__ . '/../vendor/autoload.php';
-
-use Studoo\Api\EcoleDirecte\Client;
-use Symfony\Component\Dotenv\Dotenv;
-
-$dotenv = new Dotenv();
-$dotenv->loadEnv(__DIR__ . '/../.env');
-
-$client = new Client([
-    "client_id" => $_ENV["CLIENT_ID"],
-    "client_secret" => $_ENV["CLIENT_SECRET"],
-]);
-
-echo "Token: {$client->fetchAccessToken()->getToken()} <br>";
-echo "ID: {} <br>";
-var_dump($client->getVieScolaire("7006"));
-
+echo "<h1>API ECOLE DIRECTE</h1>";
+echo "<a href='exampleForDotenv.php'>Exemple avec un fichier DotEnv (.env)</a><br>";
+echo "<a href='exampleForForm.php'>Exemple avec un formulaire</a><br>";
